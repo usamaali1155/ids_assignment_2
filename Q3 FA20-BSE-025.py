61dd76f7bee182fa9e40297be5d692e0fd057bba
Usama Ali <ua2543014@gmail.com>
2024-03-28 23:51:40 +0500

    Update Q3 FA20-BSE-025.py

--- Q3 FA20-BSE-025.py
+++ Q3 FA20-BSE-025.py
@@ -1,4 +1,14 @@
-import pandas as pd
+# 28 March 24
+# CSC461 – Assignment2 – IDS – Data Visualization
+# Usama Ali
+# FA20-BSE-025
+#Question2: [CLO-2] - [Bloom Taxonomy Level: <Applying>]
+#Using the world population dataset (from Q1)
+#• Show the population of 10 least populous countries in 2015 using a chart.
+#• Calculate the change in population of Pakistan, India, United States, and United Kingdom from 1970
+#to 2010 and show the population change (in millions) using a chart.
+#• Calculate the Pakistan population growth between 2010-2020 and then show the data using a chart.
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 
