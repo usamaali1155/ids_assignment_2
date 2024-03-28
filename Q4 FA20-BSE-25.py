66f7133a9c7e177949ac5e174c1370aa2b7f3c18
Usama Ali <ua2543014@gmail.com>
2024-03-28 23:52:27 +0500

    Update Q4 FA20-BSE-25.py

--- Q4 FA20-BSE-25.py
+++ Q4 FA20-BSE-25.py
@@ -1,3 +1,14 @@
+# 28 March 24
+# CSC461 – Assignment2 – IDS – Data Visualization
+# Usama Ali
+# FA20-BSE-025
+#Question3: [CLO-2] - [Bloom Taxonomy Level: <Applying>]
+#The diamonds dataset contains 53,000 records with various attributes like carat, cut, color, clarity, price etc.
+#Plot the relationship between ‘carat’ and ‘price’ of diamonds using a chart. Because it’s a large dataset, just
+#plot the diamonds with a ‘clarify’ = ‘SI2’ and ‘color’ = ‘E’. Use the values of the ‘cut’ for colors in the plot.
+#Make appropriate modifications to the chart title, axis titles, legend, figure size, font size, colors etc. to make
+#the chart readable and visually appealing.
+
 import pandas as pd
 import folium
 nuclear_waste_df = pd.read_csv('./nuclear_waste_sites.csv')
